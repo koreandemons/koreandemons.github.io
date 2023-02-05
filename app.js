d2014b46c1b5000de37f561c45a989e305c5e376
koreandemons <124409216+koreandemons@users.noreply.github.com>
2023-02-05 14:52:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-document.addEventListener('contextmenu', event => event.preventDefault());
+window.addEventListener('contextmenu', function (e) {
+  console.log('context menu disabled');
+  e.preventDefault();
+}, false);
 
+document.addEventListener('mouseup', function (e) {
+  if (e.button === 2) {
+    console.log('right-click enabled');
+  }
+}, false);
