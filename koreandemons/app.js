ab39af3f9e4df9cb87dcc1ea2e816f79bd20794e
koreandemons <124409216+koreandemons@users.noreply.github.com>
2023-02-05 14:34:17 +0100

    updated

--- koreandemons/app.js
+++ koreandemons/app.js
@@ -1,1 +1,15 @@
+
 document.addEventListener('contextmenu', event => event.preventDefault());
+window.addEventListener("resize", fixWindow);
+
+var canvas;
+var ctx;
+var scale = 0.12;
+
+function fixWindow() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  logo.x = Math.floor(Math.random() * canvas.width * scale);
+  logo.y = Math.floor(Math.random() * canvas.height * scale);
+}
+
